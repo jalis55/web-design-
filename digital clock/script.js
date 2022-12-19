e2ee43e1edb1b0b57b6c090900b8747405224646
jalis55 <jalismahamud2055@gmail.com>
2022-12-19 19:10:58 +0600

    digital clock

--- digital clock/script.js
+++ digital clock/script.js
@@ -3,6 +3,7 @@ const hourEl=document.getElementById("hour")
 const minutesEl=document.getElementById("minutes");
 const secondEl=document.getElementById("seconds");
 const ampmEl=document.getElementById("ampm");
+const instructionEl=document.getElementById("ins")
 
 function digitalClock(){
     let hour=new Date().getHours();
@@ -11,16 +12,23 @@ function digitalClock(){
     console.log(minutes);
     let seconds=new Date().getSeconds();
     let ampm="AM"
+    
 
     if (hour > 12){
         hour=hour -12;
         ampm="PM"
     }
     hour=hour<10 ? "0" +hour :hour;
+    minutes=minutes<10 ? "0" +minutes :minutes;
     hourEl.innerText=hour;
     minutesEl.innerText=minutes;
     secondEl.innerText=seconds;
     ampmEl.innerText=ampm;
+
+    if(hour < 8 && ampm=="PM"){
+        instructionEl.innerText="good evening";
+    }
+
     setTimeout(()=>{
         digitalClock();
     },1000)
