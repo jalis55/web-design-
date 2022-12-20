a6ec266c88959a5223262501adcdfbe5327c2ee3
jalis55 <jalismahamud2055@gmail.com>
2022-12-20 19:50:41 +0600

    bugs

--- digital clock 2/script.js
+++ digital clock 2/script.js
@@ -17,8 +17,12 @@ function digitalClock(){
 
     h=h<10 ? "0" + h :h;
     m=m<10 ? "0" + m :m;
-    s=s<10 ? "0" + s :s
+    s=s<10 ? "0" + s :s;
 
+    if (h>7 && ampm=="PM"){
+        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1475738972911-5b44ce984c42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')";
+
+    }
     hourEl.innerText=h;
     minutesEl.innerText=m;
     secondsEl.innerText=s;
