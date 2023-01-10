6b23a5a5935763c48e07efcf1a8a9985443c2315
jalis55 <jalismahamud2055@gmail.com>
2023-01-10 12:29:27 +0600

    delete works

--- todo apps/scripts.js
+++ todo apps/scripts.js
@@ -3,40 +3,35 @@ let task=document.querySelector("#task");
 let ol=document.querySelector("ol");
 
 
-function deleteTask(e){
-	let task=e.target;
-	let parentTask=task.parentElement;
-	console.log(parentTask);
-	parentTask.remove();
-	
-	
-}
+
 function todoapp(e){
     e.preventDefault();
     let taskValue=task.value;
     
-    if (taskValue ==""){
+    if (task.value ==""){
 
         alert("Field can not be empty");
         return false;
     }
-    else{
-        let li=document.createElement("li");
-        let p=document.createElement("p");
-        p.innerText=taskValue;
-        let deleteBtn=document.createElement("button");
-        deleteBtn.id="delete";
-        deleteBtn.innerText="Delete";
-        li.appendChild(p);
-        li.appendChild(deleteBtn);
-        ol.appendChild(li);
-
-        
-        
-    }
+    
+    let li=document.createElement("li");
+    let p=document.createElement("p");
+    p.innerText=taskValue;
+    let deleteBtn=document.createElement("button");
+    deleteBtn.id="delete";
+    deleteBtn.innerText="Delete";
+    li.appendChild(p);
+    li.appendChild(deleteBtn);
+    ol.appendChild(li);
+    p.addEventListener("click",()=>{
+        p.style.textDecoration="line-through";
+    })
+    deleteBtn.addEventListener("click",function(){
+        let deleteItem=deleteBtn.parentElement;
+        deleteItem.remove();
+    })
     task.value=""
 }
 
 form.addEventListener("submit",todoapp);
-let TaskItem=document.querySelector("#delete");
-TaskItem.addEventListener("click",deleteTask);
+
